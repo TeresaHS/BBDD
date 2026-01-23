66ab565c6bcd5bb4917e706f2b1e5badedb405e4
TeresaHS <teresahernandezsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-01-23 19:14:32 +0100

    Update and rename BORRAR_BBDD_Potencial_Digital_SQL.sql to BORRAR_BBDD_Potencial_Digital_SQL.sql

--- TAR-05.01/BORRAR_BBDD_Potencial_Digital_SQL.sql
+++ TAR-05.01/BORRAR_BBDD_Potencial_Digital_SQL.sql
@@ -7,4 +7,5 @@ BEGIN
     ALTER DATABASE Potencial_Digital_SQL SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE Potencial_Digital_SQL;
     PRINT 'La consulta ha funcionado correctamente :)';
+
 END
