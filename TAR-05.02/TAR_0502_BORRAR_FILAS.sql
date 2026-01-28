d31d90701d204e59bea2c10b1963d2880d66dded
TeresaHS <teresahernandezsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-01-28 19:44:30 +0100

    Update TAR_0502_BORRAR_FILAS.sql

--- TAR-05.02/TAR_0502_BORRAR_FILAS.sql
+++ TAR-05.02/TAR_0502_BORRAR_FILAS.sql
@@ -5,8 +5,12 @@ DELETE FROM fechas_charlas;
 DELETE FROM fechas;
 DELETE FROM salas;
 DELETE FROM pabellones;
+DELETE FROM colegios_actividades;
 DELETE FROM charlas;
 DELETE FROM areas;
+
+DELETE FROM colegios;
+
 
 /* El delete borrar las filas, pero hay que recordar que el contador se mantiene igual, no se reinicia. 
 Lo sigueitn me permite volver a la base 0 para comenzar introduciendo filas desde 1 */
@@ -15,3 +19,4 @@ DBCC CHECKIDENT ('areas', RESEED, 0);
 DBCC CHECKIDENT ('charlas', RESEED, 0);
 DBCC CHECKIDENT ('pabellones', RESEED, 0);
 DBCC CHECKIDENT ('salas', RESEED, 0);
+DBCC CHECKIDENT ('colegios_actividades', RESEED, 0);
